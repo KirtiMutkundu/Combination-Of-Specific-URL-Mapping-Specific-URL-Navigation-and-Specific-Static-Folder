0d53019fdb894477ee7372939d331c97c5194261
KirtiMutkundu <kirtimutkundureddy2007@gmail.com>
2024-04-14 15:53:50 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def clothes(request):
+    return render(request,'clothes.html')
+
+def shoes(request):
+    return render(request,'shoes.html')
